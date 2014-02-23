a83a37a95778e322fe82ef45c4f606b74eafb2aa
neil patel <neil@myindianweddingapp.com>
2014-02-23 19:59:30 +0000

    first commit

---
+++ app/views/listings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @listing, :name, :description, :price, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo::Application.config.secret_key_base = 'd8816f6356182ff2cc9ca106c0d1c9cc6c6b4a710f572c8eed000b5e488dfd99f7daf0f4e0902cd72ac0b66d355d2df95c112cf696053f2e09129f1771732d0c'
